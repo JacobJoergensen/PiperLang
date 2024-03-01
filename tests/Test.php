48b7f6f7624f5bc6db33a59805bd990a9db36129
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2024-03-01 22:59:55 +0100

    Update Test.php
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

--- tests/Test.php
+++ tests/Test.php
@@ -4,7 +4,7 @@
     use PHPUnit\Framework\TestCase;
     
     final class ExceptionTest extends TestCase {
-        public function testException(): void {
-            $this -> expectException(InvalidArgumentException::class);
-        }
+        //public function testException(): void {
+            //$this -> expectException(InvalidArgumentException::class);
+        //}
     }
