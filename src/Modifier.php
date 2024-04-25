aff30d9551fdbfe3ec2b5eab455923e24c8e174b
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2024-04-25 18:05:01 +0200

    Add modifier feature
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

---
+++ src/Modifier.php
@@ -0,0 +1,9 @@
+<?php
+    namespace PiperLang;
+
+    class Modifier extends PiperLang {
+        public function __construct() {
+            parent::__construct();
+            // ADD CUSTOM CODE HERE.
+        }
+    }
