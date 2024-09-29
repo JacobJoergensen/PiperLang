3754b3ed70d636d7c2890b67b322db3f63cc746b
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2024-09-29 18:17:28 +0200

    Update Tracking - 1.2.0 (#58)
    * Some improvements
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update PiperLang.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update PiperLang.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update .editorconfig
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update PiperLang.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update Modifier.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update PiperLang.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update PiperLang.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Add files via upload
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update PiperLang.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update PiperLang.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Delete tests/Test.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Add files via upload
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update PiperLangTest.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update PiperLang.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update PiperLangTest.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update PiperLangTest.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update PiperLangTest.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    * Update PiperLangTest.php
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

--- src/PiperLang.php
+++ src/PiperLang.php
@@ -15,7 +15,7 @@
      * @package    PiperLang\PiperLang
      * @author     Jacob Jørgensen
      * @license    MIT
-     * @version    1.1.0
+     * @version    1.2.0
      */
     class PiperLang {
         /**
@@ -144,21 +144,21 @@
          */
         public function getInfo(): array {
             return [
-                'Debug Status' => $this -> debug,
-                'Hooks List' => $this -> hooks,
-                'Current Locale' => $this -> current_locale,
-                'Default Locale' => $this -> default_locale,
-                'Supported Locales' => $this -> supported_locales,
-                'Path to Locales' => $this -> locale_path,
-                'Locale File Extension' => $this -> locale_file_extension,
-                'Loaded Locales' => $this -> loaded_locales,
-                'Variable Pattern' => $this -> variable_pattern,
-                'Plural Rules' => $this -> plural_rules,
+                'Debug Status' => $this -> debug ?? false,
+                'Hooks List' => $this -> hooks ?? [],
+                'Current Locale' => $this -> current_locale ?? '',
+                'Default Locale' => $this -> default_locale ?? 'en',
+                'Supported Locales' => $this -> supported_locales ?? ['en'],
+                'Path to Locales' => $this -> locale_path ?? '/locales/',
+                'Locale File Extension' => $this -> locale_file_extension ?? 'json',
+                'Loaded Locales' => $this -> loaded_locales ?? [],
+                'Variable Pattern' => $this -> variable_pattern ?? '/{{(.*?)}}/',
+                'Plural Rules' => $this -> plural_rules ?? [],
                 'HTTP Accept Locale' => $this -> getHttpAcceptLanguage(),
-                'Session Enabled' => $this -> session_enabled,
-                'Session Key' => $this -> session_key,
-                'Cookie Enabled' => $this -> cookie_enabled,
-                'Cookie Key' => $this -> cookie_key
+                'Session Enabled' => $this -> session_enabled ?? true,
+                'Session Key' => $this -> session_key ?? 'locale',
+                'Cookie Enabled' => $this -> cookie_enabled ?? false,
+                'Cookie Key' => $this -> cookie_key ?? 'site_locale',
             ];
         }
 
@@ -213,7 +213,7 @@
          * @return ?string - THE CURRENT LOCALE CODE.
          */
         public function getLocale(): ?string {
-            return $this -> current_locale;
+            return $this -> current_locale ?? $this -> default_locale;
         }
 
         /**
@@ -251,10 +251,8 @@
                     throw new RuntimeException('Failed to set the cookie, headers were already sent');
                 }
 
-                if (!setcookie($this -> cookie_key, $this -> current_locale, time() + (86400 * 30), "/")) {
-                    if ($this -> debug) {
-                        throw new RuntimeException('Failed to set locale in cookie');
-                    }
+                if (!setcookie($this -> cookie_key, $this -> current_locale, time() + (86400 * 30), "/") && $this -> debug) {
+                    throw new RuntimeException('Failed to set locale in cookie');
                 }
             }
         }
@@ -356,7 +354,11 @@
          * @throws RuntimeException - THROWN IF THERE ARE ISSUES IN READING THE FILE OR MISSING 'variables' DATA.
          */
         public function loadFile(string $locale): array {
-            $locale_file = $_SERVER['DOCUMENT_ROOT'] . $this -> locale_path . $locale . '.' . $this -> locale_file_extension;
+            if (isset($_SERVER['DOCUMENT_ROOT']) && is_string($_SERVER['DOCUMENT_ROOT'])) {
+                $locale_file = $_SERVER['DOCUMENT_ROOT'] . $this -> locale_path . $locale . '.' . $this -> locale_file_extension;
+            } else {
+                throw new RuntimeException('DOCUMENT_ROOT is not set or is not a valid string.');
+            }
 
             if (!file_exists($locale_file)) {
                 if ($locale !== $this -> default_locale) {
@@ -399,23 +401,20 @@
 
             $variables = $locale_nodes['variables'] ?? [];
 
-            /**
-             * @phpstan-ignore-next-line
-             */
-            if (is_null($variables)) {
+            if (!is_array($variables)) {
                 throw new RuntimeException("Locale file $locale_file does not contain required 'variables' data");
             }
 
-            if (is_array($variables)) {
-                unset($locale_nodes['variables']);
+            $variables = array_map(function ($value) {
+                return is_scalar($value) ? (string) $value : '';
+            }, $variables);
 
-                foreach ($locale_nodes as $key => $value) {
-                    if (is_string($value)) {
-                        try {
-                            $locale_nodes[$key] = $this -> replaceVariables($value, $variables);
-                        } catch (RuntimeException $exception) {
-                            throw new RuntimeException("Error replacing variable '$key' in locale file: $locale_file", 0, $exception);
-                        }
+            foreach ($locale_nodes as $key => $value) {
+                if (is_string($value)) {
+                    try {
+                        $locale_nodes[$key] = $this -> replaceVariables($value, $variables);
+                    } catch (RuntimeException $exception) {
+                        throw new RuntimeException("Error replacing variable '$key' in locale file: $locale_file", 0, $exception);
                     }
                 }
             }
@@ -453,10 +452,6 @@
          * @throws RuntimeException - THROWN IF NUMBER FORMATTING FAILS.
          */
         public function formatNumber(float $number): string {
-            if (!is_numeric($number)) {
-                throw new InvalidArgumentException('Not a valid number for formatting.');
-            }
-
             if ($this -> current_locale === null) {
                 throw new InvalidArgumentException('Current locale not set.');
             }
@@ -486,10 +481,6 @@
          * @throws RuntimeException - THROWN IF CURRENCY FORMATTING FAILS.
          */
         public function formatCurrency(float $amount, string $currency, bool $show_symbol = false): string {
-            if (!is_numeric($amount)) {
-                throw new InvalidArgumentException('Not a valid amount for currency formatting.');
-            }
-
             if (!preg_match("/^[A-Z]{3}$/", $currency) && $this -> debug) {
                 throw new InvalidArgumentException('Not a valid ISO 4217 currency code.');
             }
