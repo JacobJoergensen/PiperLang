b09810ea02270f48dba77f387de6e1709b81e586
Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>
2024-09-29 16:16:07 +0200

    Add files via upload
    Signed-off-by: Jacob Joergensen <96596794+JacobJoergensen@users.noreply.github.com>

---
+++ tests/ModifierTest.php
@@ -0,0 +1,39 @@
+<?php
+    namespace Tests;
+
+    use PHPUnit\Framework\TestCase;
+
+    use PiperLang\PiperLang;
+    use PiperLang\Modifier;
+
+    class ModifierTest extends TestCase {
+        public function testModifierInstance(): void {
+            $modifier = new Modifier();
+
+            $this -> assertInstanceOf(Modifier::class, $modifier);
+            $this -> assertInstanceOf(PiperLang::class, $modifier);
+        }
+
+        public function testModifierInheritsProperties(): void {
+            $modifier = new Modifier();
+
+            $this -> assertIsArray($modifier -> hooks);
+            $this -> assertIsBool($modifier -> debug);
+            $this -> assertEquals('en', $modifier -> default_locale);
+            $this -> assertEquals('/locales/', $modifier -> locale_path);
+            $this -> assertIsArray($modifier -> supported_locales);
+            $this -> assertContains('en', $modifier -> supported_locales);
+        }
+
+        public function testModifierInheritsMethods(): void {
+            $modifier = new Modifier();
+
+            $info = $modifier -> getInfo();
+
+            $this -> assertIsArray($info);
+
+            $this -> assertArrayHasKey('Debug Status', $info);
+            $this -> assertArrayHasKey('Current Locale', $info);
+            $this -> assertArrayHasKey('Default Locale', $info);
+        }
+    }
